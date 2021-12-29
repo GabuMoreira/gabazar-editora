007ef61d7cb9cc9086d841c5db058402310d5853
Gabriel Moreira <joaogmrodrigues@outlook.com>
2021-12-29 15:15:49 -0300

    ✨ START ✨
    POM + MVN

---
+++ src/main/java/me/gabu/gabazar/editoras/EditorasApplication.java
@@ -0,0 +1,13 @@
+package me.gabu.gabazar.editoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/gabu/gabazar/editoras/EditorasApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabu.gabazar.editoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
