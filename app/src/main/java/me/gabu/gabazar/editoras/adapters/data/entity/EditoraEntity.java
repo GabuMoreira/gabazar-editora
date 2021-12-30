6a32dc3fd7aa7779a840cb8186431e5a4dd50536
Gabriel Moreira <joaogmrodrigues@outlook.com>
2021-12-30 12:44:19 -0300

    ➕ H2
    Adição de biblioteca de banco de dados em memoria (para testes)

--- app/src/main/java/me/gabu/gabazar/editoras/adapters/data/entity/EditoraEntity.java
+++ app/src/main/java/me/gabu/gabazar/editoras/adapters/data/entity/EditoraEntity.java
@@ -1,8 +1,24 @@
 package me.gabu.gabazar.editoras.adapters.data.entity;
 
-import javax.persistence.Entity;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 public class EditoraEntity {
+
+    @Id
+    private String id;
+    private String nome;
+    private String site;
+    private Date dataCriacao;
+    private Date dataAlteracao;
+    private String usuario;
 
 }
